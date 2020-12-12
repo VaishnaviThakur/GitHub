bf3e664631ecc6c93d5a6dfe54239bb662df49b2
VaishnaviT <vaishnavit2309@gmail.com>
2020-12-12 17:21:00 +0530

    jenkins

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { ForthComponent } from './forth/forth.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
+
+const routes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
+  { path: 'third', component: ThirdComponent },
+  { path: 'forth', component: FourthComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
